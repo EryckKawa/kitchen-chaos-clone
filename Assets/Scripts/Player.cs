831d90b79d83a7401b1c0f08ec5a5888a8e59be7
Eryck Kawa <okazakiall114@gmail.com>
2024-04-12 22:57:24 -0300

    Update Player.cs

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float movSpeed;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputSystem = new Vector2(0, 0);
@@ -30,8 +32,15 @@ public class Player : MonoBehaviour
 
         Vector3 movDir = new Vector3(inputSystem.x, 0f, inputSystem.y);
         transform.position += movSpeed * Time.deltaTime * movDir;
-        
+
+        isWalking = movDir != Vector3.zero;
+
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, movDir, Time.deltaTime * rotateSpeed);
+    }
+
+    public bool IsWalking()
+    {
+        return isWalking;
     }
 }

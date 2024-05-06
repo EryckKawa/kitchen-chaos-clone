a9ac5666c6424398205b016d1d8828acef90cb8a
Eryck Kawa <okazakiall114@gmail.com>
2024-05-05 23:46:47 -0300

    add stove counter and trash counter
    add stove counter and frying kitchen object mechanic

---
+++ Assets/Scripts/Counters/ClearCounter.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ClearCounter : BaseCounter
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    public override void Interact(Player player)
+    {
+        //Bancada Vazia
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+        }
+        else
+        {
+            //Player carregando objeto
+            if (player.HasKitchenObject())
+            {
+
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+}
